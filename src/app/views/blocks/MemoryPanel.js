4d82bd9c8b838c958a429eecac260a335be140e1
Дмитрий Пацура <zaets28rus@gmail.com>
2014-06-04 10:39:12 +0400

    MemoryPanel select none

--- src/app/views/blocks/MemoryPanel.js
+++ src/app/views/blocks/MemoryPanel.js
@@ -1,7 +1,4 @@
 ns('App.views.blocks.MemoryPanel', Backbone.View.extend({
-    events: {
-        'scroll .memory': 'memoryScroll'
-    },
     totalCells: 40,
     initialize: function (opts) {
         this.$el = opts.$el;
@@ -22,9 +19,6 @@ ns('App.views.blocks.MemoryPanel', Backbone.View.extend({
         this.$el.html(html);
         this.$el.find('.memory').css('width', ((this.totalCells+1)*this.totalCells) + 'px');
         this.$el.scrollLeft((((this.totalCells+1)*40)/4)+20);
-    },
-    memoryScroll: function(event) {
-
     },
     active: function(positionId) {
         this.getCell(positionId).addClass('active');
